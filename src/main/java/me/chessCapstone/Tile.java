4e85767edc3e74d7e5b3b7a0fe336992e65c30e5
TheGomez01 <159203345+TheGomez01@users.noreply.github.com>
2024-11-21 11:49:22 -0600

    Update Tile.java
    Added source code documentation to Tile.

--- src/main/java/me/chessCapstone/Tile.java
+++ src/main/java/me/chessCapstone/Tile.java
@@ -1,41 +1,55 @@
 package me.chessCapstone;
 
+//This class is meant to represent individual squares.
 public class Tile {
 
+    //Each Tile has it's own row and column.
     private int row, col;
 
+    //Constructor for a Tile object. Requires two integer arguments for the column and row.
     public Tile(int col, int row) {
         this.col = col;
         this.row = row;
     }
 
+    //Returns the column of the Tile.
     public int getCol() {
         return col;
     }
 
+    //Set the column of the Tile.
     public void setCol(int col) {
         this.col = col;
     }
 
+    //Return the row of the Tile.
     public int getRow() {
         return row;
     }
 
+    //Set the row of the Tile.
     public void setRow(int row) {
         this.row = row;
     }
 
     @Override
+    //Tiles are now considered equal if they share a row and column, not only a space in memory.
     public boolean equals(Object obj) {
+        
+        //Checks to see if they share a space in memory.
         if(this == obj) {
             return true;
         }
 
+        //Checks to see if the passed object is not aninstance of the Tile class.
         if(!(obj instanceof Tile)) {
             return false;
         }
 
+        //Object is downcasted into a Tile class
         Tile tile = (Tile) obj;
+
+        //If the row and column of both Tiles are the same, they are equal and true is returned.
         return this.row == tile.row && this.col == tile.col;
     }
 
