f0703c4f841596a0cff87bf3ce01094d8d65ac5a
TheGomez01 <159203345+TheGomez01@users.noreply.github.com>
2024-11-21 11:26:46 -0600

    Update Queen.java
    Added source code documentation to Queen.

--- src/main/java/me/chessCapstone/Queen.java
+++ src/main/java/me/chessCapstone/Queen.java
@@ -8,25 +8,32 @@ public class Queen extends Piece {
         super("queen", color);
     }
 
-    //Given a target column and row, this method ensures that the target is valid given Queen movement rules.
+    //This method ensures that the target square is valid given Queen movement rules.
     public boolean isValidQueenMove(int endCol, int endRow, String[][] boardCurrent) {
 
+        //Calculates the difference between the current and target squares.
         int colDiff = Math.abs(endCol - getCol());
         int rowDiff = Math.abs(endRow - getRow());
 
-        // 수직 이동
+        //Checks to see if the target square can be reached vertically.
         if (getCol() == endCol && getRow() != endRow) {
+             //Target square can be reached. The path is then checked.
             return isPathClear(endCol, endRow, boardCurrent);
         }
-        // 수평 이동
+            
+        //Checks to see if the target square can be reached horizontally.
         else if (getRow() == endRow && getCol() != endCol) {
+            //Target square can be reached. The path is then checked.
             return isPathClear(endCol, endRow, boardCurrent);
         }
-        // 대각선 이동
+            
+        //Checks to see if the target square can be reached diagonally.
         else if (colDiff == rowDiff) {
+            //Target square can be reached. The path is then checked.
             return isPathClear(endCol, endRow, boardCurrent);
         }
 
+        //The target square cannot be reached by Queen movement rules.
         return false;
     }
 
