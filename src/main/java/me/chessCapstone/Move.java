d09cdf28cfe041b0db44f2f5b572c69ac2f12ea7
Joe Johnson <148178752+Joe-Johnson-23@users.noreply.github.com>
2024-11-24 20:46:05 -0600

    added the remainder of my documentation to chessGame, ChessEngine, customAI, and Move

--- src/main/java/me/chessCapstone/Move.java
+++ src/main/java/me/chessCapstone/Move.java
@@ -1,10 +1,15 @@
 package me.chessCapstone;
 
+//Represents a chess move with starting position, ending position,
+//and piece information
+//helps with move validation and applying moves for custom AI
 public class Move {
+    //Final fields to ensure immutability
     public final int startCol, startRow, endCol, endRow;
     public final String piece;
 
     public Move(int startCol, int startRow, int endCol, int endRow, String piece) {
+        //Initialize fields
         this.startCol = startCol;
         this.startRow = startRow;
         this.endCol = endCol;
